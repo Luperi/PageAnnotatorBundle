0294e24a44e1624b7211084eacc33f7c3ea6bffc
Luca Perico <pericoluca1993@gmail.com>
2015-03-23 18:30:02 +0100

    parameters.yml add

--- DependencyInjection/PageAnnotatorExtension.php
+++ DependencyInjection/PageAnnotatorExtension.php
@@ -7,11 +7,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class PageAnnotatorExtension extends Extension
 {
     /**
@@ -24,5 +19,6 @@ class PageAnnotatorExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('parameters.yml');
     }
 }
