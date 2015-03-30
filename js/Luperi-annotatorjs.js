bbae6db71345ded160c1bcc7d5f418e8f5a91a15
Luca Perico <pericoluca1993@gmail.com>
2015-03-30 11:01:29 +0200

    annotateWithFixedValues()

--- js/Luperi-annotatorjs.js
+++ js/Luperi-annotatorjs.js
@@ -1,6 +1,6 @@
 /*
  * File creato da Luca Perico
- * Include funzioni utili nell'utilizzo di Annotator
+ * Include le funzioni necessarie per l'utilizzo di annotator con Symfony
  * Support at: https://www.npmjs.com/package/annotator
  */
 
@@ -64,7 +64,23 @@ function setAnnotatorLanguage(lang){
             break;
         case 'es':
             $('.annotator-cancel').html("Cancela");
-            $('.annotator-save').html("Salvar");
+            $('.annotator-save').html("Guardar");
+            break;
+        case 'de':
+            $('.annotator-cancel').html("Annulliert");
+            $('.annotator-save').html("Sparen");
             break;
     }
+}
+
+function annotateWithFixedValues(values){
+    $('#annotator-field-0').parent().empty();
+    $('#annotator-field-1').parent().css({'padding':'5px'});
+    var text = "<span style='color:black; font-size: 20px;'>Questo è: </span><select id='comment-selection' style='color:black; font-size: 15px;' onchange='writeComment(this)'><option value='null' selected='selected'>- seleziona -</option>";
+
+    values.forEach(function(entry) {
+        text = text + "<option value='" + entry.toLowerCase() + "'>" + entry + "</option>";
+    });
+
+    $('#annotator-field-1').parent().html(text);
 }
