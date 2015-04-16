b371ebf44da4f9b0ef29dd5bc20b0d3d2eaaee3a
Luca Perico <pericoluca1993@gmail.com>
2015-04-16 16:57:29 +0200

    fix response

--- Controller/AnnotationController.php
+++ Controller/AnnotationController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Luperi\PageAnnotatorBundle\Entity\Annotation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AnnotationController extends Controller
 {
